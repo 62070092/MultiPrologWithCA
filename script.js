039a0042a614ff15a86df60d79050a9e662c363c
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-07 23:44:36 +0700

    /

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-let info = "ในประเทศ"
+let info = "ข้อมูล1"
 
 function swapInfo(){
-    if(info === "ในประเทศ"){
+    if(info === "ข้อมูล1"){
         document.querySelectorAll(".info-1")[0].dataset.swap="0";
         document.querySelectorAll(".info-2")[0].dataset.swap="1";
-        info = "ต่างประเทศ"
+        info = "ข้อมูล2"
     }
     else{
         document.querySelectorAll(".info-1")[0].dataset.swap="1";
         document.querySelectorAll(".info-2")[0].dataset.swap="0";
-        info = "ในประเทศ"
+        info = "ข้อมูล1"
     }
+}
+
+function swapInfo_2(){
+    if(info === "ข้อมูล1"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+        info = "ข้อมูล2"
+    }
+    else if(info === "ข้อมูล2"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="1";
+        info = "ข้อมูล3"
+    }
+    else if(info === "ข้อมูล3"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="1";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+        info = "ข้อมูล1"
+    }
+
 }
