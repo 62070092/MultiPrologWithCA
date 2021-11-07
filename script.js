1e60bdd49efbb729e949a12702cead123d8224db
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-07 22:21:53 +0700

    /

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
+let info = "ในประเทศ"
 
-
-function swapInfo(direction){
-    if(direction == "right"){
-        let country = document.querySelector(".country");
-        let abroad = document.querySelector(".abroad");
+function swapInfo(){
+    if(info === "ในประเทศ"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        info = "ต่างประเทศ"
+    }
+    else{
+        document.querySelectorAll(".info-1")[0].dataset.swap="1";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        info = "ในประเทศ"
     }
 }
-
-var slide_strip = $('.infos');
-var slides = slide_strip.find('> *');
-
-var next = $('.btn-right');
-var prev = $('.btn-left');
-
-next.on('click', swapInfo.bind(null, 'next'));
-prev.on('click',swapInfo);
-
-function swapInfo(direction){
-    var swap = parseInt(slide_strip.attr('data-swap'), 5);
-
-    if(direction == 'next'){
-            swap = Math.min(infos.length, swap + 1);
-    } else {
-            swap = Math.max(swap - 1, 0);
-    }
-
-    slide_strip.attr('data-swap', swap);
-    };
