d319e1f0ab32116e4214ef7c6ab29af375de5f17
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-16 14:09:02 +0700

    icon

--- script.js
+++ script.js
@@ -22,18 +22,30 @@ function swapInfo_2(){
         document.querySelectorAll(".info-1")[0].dataset.swap="0";
         document.querySelectorAll(".info-2")[0].dataset.swap="1";
         document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
         info = "ข้อมูล2"
     }
     else if(info === "ข้อมูล2"){
         document.querySelectorAll(".info-1")[0].dataset.swap="0";
         document.querySelectorAll(".info-2")[0].dataset.swap="0";
         document.querySelectorAll(".info-3")[0].dataset.swap="1";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="1";
         info = "ข้อมูล3"
     }
     else if(info === "ข้อมูล3"){
         document.querySelectorAll(".info-1")[0].dataset.swap="1";
         document.querySelectorAll(".info-2")[0].dataset.swap="0";
         document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
         info = "ข้อมูล1"
     }
 
