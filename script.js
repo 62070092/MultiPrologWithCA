75446d7dfaa971299700cd6d91af107f0260142b
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-16 12:42:24 +0700

    dataset-icon

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function swapInfo(){
     if(info === "ข้อมูล1"){
         document.querySelectorAll(".info-1")[0].dataset.swap="0";
         document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="1";
         info = "ข้อมูล2"
     }
     else{
         document.querySelectorAll(".info-1")[0].dataset.swap="1";
         document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".icon_1")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
         info = "ข้อมูล1"
     }
 }
