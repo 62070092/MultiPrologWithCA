3ac7928f99e06620da88d1817ab3b24008ef8dfd
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-23 00:01:30 +0700

    /

--- storage.js
+++ storage.js
@@ -1,8 +1,11 @@
 function submitComment(){
+
     const name = document.getElementById('name').value;
     const comment = document.getElementById('comment').value;
+
     localStorage.setItem("NAME", name);
     localStorage.setItem("COMMENT", comment);
+    
     return;
 
 }
