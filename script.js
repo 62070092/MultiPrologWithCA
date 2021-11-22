5dbf8da90253b47117ba59c9b6bcd1a59823b8d8
Tanya Sisook <62070092@it.kmitl.ac.th>
2021-11-22 17:19:30 +0700

    /

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ function swapInfo_2(){
         document.querySelectorAll(".icon_2")[0].dataset.icon="1";
         document.querySelectorAll(".icon_3")[0].dataset.icon="0";
 
-        document.querySelectorAll(".bar")[0].dataset.bar="2";
+        document.querySelectorAll(".bar_1")[0].dataset.bar="2";
+
         info = "ข้อมูล2"
     }
     else if(info === "ข้อมูล2"){
@@ -42,7 +43,8 @@ function swapInfo_2(){
         document.querySelectorAll(".icon_2")[0].dataset.icon="0";
         document.querySelectorAll(".icon_3")[0].dataset.icon="1";
 
-        document.querySelectorAll(".bar")[0].dataset.bar="3";
+        document.querySelectorAll(".bar_1")[0].dataset.bar="3";
+
         info = "ข้อมูล3"
     }
     else if(info === "ข้อมูล3"){
@@ -54,7 +56,128 @@ function swapInfo_2(){
         document.querySelectorAll(".icon_2")[0].dataset.icon="0";
         document.querySelectorAll(".icon_3")[0].dataset.icon="0";
 
-        document.querySelectorAll(".bar")[0].dataset.bar="1";
+        document.querySelectorAll(".bar_1")[0].dataset.bar="1";
+
+        info = "ข้อมูล1"
+    }
+
+}
+
+function swapInfo_3(){
+    if(info === "ข้อมูล1"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+        document.querySelectorAll(".bar_2")[0].dataset.bar="2";
+        info = "ข้อมูล2"
+    }
+    else if(info === "ข้อมูล2"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="1";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="1";
+
+        document.querySelectorAll(".bar_2")[0].dataset.bar="3";
+        info = "ข้อมูล3"
+    }
+    else if(info === "ข้อมูล3"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="1";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+        document.querySelectorAll(".bar_2")[0].dataset.bar="1";
+        info = "ข้อมูล1"
+    }
+
+}
+
+function swapInfo_4(){
+    if(info === "ข้อมูล1"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+        document.querySelectorAll(".bar_3")[0].dataset.bar="2";
+        info = "ข้อมูล2"
+    }
+    else if(info === "ข้อมูล2"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="1";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="1";
+
+        document.querySelectorAll(".bar_3")[0].dataset.bar="3";
+        info = "ข้อมูล3"
+    }
+    else if(info === "ข้อมูล3"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="1";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+        document.querySelectorAll(".bar_3")[0].dataset.bar="1";
+        info = "ข้อมูล1"
+    }
+
+}
+
+function swapInfo_5(){
+    if(info === "ข้อมูล1"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="1";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+
+        info = "ข้อมูล2"
+    }
+    else if(info === "ข้อมูล2"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="0";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="1";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="1";
+
+
+        info = "ข้อมูล3"
+    }
+    else if(info === "ข้อมูล3"){
+        document.querySelectorAll(".info-1")[0].dataset.swap="1";
+        document.querySelectorAll(".info-2")[0].dataset.swap="0";
+        document.querySelectorAll(".info-3")[0].dataset.swap="0";
+
+        document.querySelectorAll(".icon_1")[0].dataset.icon="1";
+        document.querySelectorAll(".icon_2")[0].dataset.icon="0";
+        document.querySelectorAll(".icon_3")[0].dataset.icon="0";
+
+
         info = "ข้อมูล1"
     }
 
